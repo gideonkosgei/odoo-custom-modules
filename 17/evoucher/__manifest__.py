207fc92ce72552947735feacff4e65f63fde3591
GKosgei <mr.gkosgei@gmail.com>
2023-12-12 13:20:06 +0300

    generate all vocuhers

--- 17/evoucher/__manifest__.py
+++ 17/evoucher/__manifest__.py
@@ -13,15 +13,27 @@
     'data': [
         'security/ir.model.access.csv',
         'data/evoucher.staff.csv',
+        'report/report.xml',
         'views/staff.xml',
         'views/menu.xml',
-        'report/evoucher_card.xml',
-        'report/evoucher_cards.xml',
-        'report/report.xml'
+        'report/evoucher_cards.xml'
     ],
     'demo': [],
     'qweb': [],
     'installable': True,
+    'assets': {
+        'web.assets_backend': [
+            'evoucher/static/src/scss/event.scss'
+        ],
+        'web.report_assets_common': [
+            '/evoucher/static/src/scss/event_badge_report.scss',
+            '/evoucher/static/src/scss/event_full_page_ticket_report.scss',
+            '/evoucher/static/src/scss/event_full_page_ticket_responsive_html_report.scss',
+        ],
+        'web.report_assets_pdf': [
+            '/evoucher/static/src/scss/event_full_page_ticket_report_pdf.scss',
+        ],
+    },
     'application': True,
     'auto_install': False
 }
